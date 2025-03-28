38bac824d73386bb8deefc4ab97af84481c254e8
KER AND HAN <shalcozbi@gmail.com>
2025-03-28 23:04:56 +0800

    pseudocode batch 6 prog 8

---
+++ batch_6/print_swapcase.py
@@ -0,0 +1,6 @@
+#Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+
+#Ask user to input a statement
+#if the letter is in lowercase, convert into upper
+#else, convert it to lowercase
+#print the result
