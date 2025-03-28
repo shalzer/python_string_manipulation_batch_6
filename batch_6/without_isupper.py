1bc697060dc8118fc63359c1a99fdb6a78e23c8c
KER AND HAN <shalcozbi@gmail.com>
2025-03-28 22:28:33 +0800

    Ask for user's input

---
+++ batch_6/without_isupper.py
@@ -0,0 +1,6 @@
+#Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+
+#Ask user to enter a text
+#Using .upper, convert the input into lower case
+#Then using .swapcase, it converts the input into upper case
+# Print the result
