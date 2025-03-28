8a3be147a47a23753ae2eac94e0816be6b1584c0
KER AND HAN <shalcozbi@gmail.com>
2025-03-28 22:09:37 +0800

    pseudocode prog 3

---
+++ batch_6/without_using_lower.py
@@ -0,0 +1,6 @@
+#Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+#Ask user to enter anything
+#Using .upper, convert the input into upper case
+#Then using .swapcase, it converts the input into lower case
+# Print the result
