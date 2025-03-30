18f3ab0256d0f1b3ae0adfb5c111ff449f53baba
KER AND HAN <shalcozbi@gmail.com>
2025-03-30 22:00:15 +0800

    pseudocode batch 6 prog 5

---
+++ batch_6/check_without_using_endswith.py
@@ -0,0 +1,7 @@
+#Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+#Ask user to input text
+#Ask user what suffix they would like to check
+#Check if the input ends with the entered suffix
+#Print if it ends with the entered suffix
+#Else, it will print "does not end with"
