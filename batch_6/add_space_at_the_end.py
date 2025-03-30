08de219b911f2640c19bdd79104172e7e768da51
KER AND HAN <shalcozbi@gmail.com>
2025-03-30 22:22:23 +0800

    pseudocode batch 6 prog 6

---
+++ batch_6/add_space_at_the_end.py
@@ -0,0 +1,6 @@
+#Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+#Ask user to input a text
+#Ask user to input the total width
+#Set space equal to width
+#Create loop that adds spaces to text until it's equal with the entered width
+#Print the final output
