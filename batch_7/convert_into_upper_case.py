b6f7b7382c7f590a68afb59ac5e568a8525f659e
KER AND HAN <shalcozbi@gmail.com>
2025-03-30 23:02:55 +0800

    pseudocode batch 7 prog 3

---
+++ batch_7/convert_into_upper_case.py
@@ -0,0 +1,6 @@
+#Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
+#Ask user to enter a text
+#Using .lower, convert the input into lower case
+#Then using .swapcase, it converts the input into upper case
+# Print the result
