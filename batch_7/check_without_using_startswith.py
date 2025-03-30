bb5cc87858a7e279c0f4f40da860d2ff0e06a7bb
KER AND HAN <shalcozbi@gmail.com>
2025-03-30 23:59:03 +0800

    pseudocode batch 7 prog 5

---
+++ batch_7/check_without_using_startswith.py
@@ -0,0 +1,7 @@
+#Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+#Ask user to input text
+#Ask user what prefix they would like to check
+#Check if the input ends with the entered prefix
+#Print if it starts with the entered prefix
+#Else, it will print "does not start with"
