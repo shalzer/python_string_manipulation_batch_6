992643e9f11017c921639ee6542265ec38115154
KER AND HAN <shalcozbi@gmail.com>
2025-03-30 23:09:44 +0800

    pseudocode batch 7 prog 4

---
+++ batch_7/convert_without_islower.py
@@ -0,0 +1,6 @@
+#Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+
+#Ask user to enter anything
+#Using .upper, convert the input into upper case
+#Then using .swapcase, it converts the input into lower case
+# Print the result
