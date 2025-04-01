f5cffb0f185f363f84125fe711657ffaa4fbc440
KER AND HAN <shalcozbi@gmail.com>
2025-04-01 23:50:39 +0800

    pseudocode batch 7 prog 9

---
+++ batch_7/find_location_without_using_index.py
@@ -0,0 +1,5 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+#Ask user for input
+#Ask for keyword to find
+#Check if keyword is in input
+#Print the index if yes
