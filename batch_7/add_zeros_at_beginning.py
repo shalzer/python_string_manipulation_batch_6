4cec722588f57d4a71e29734ec82e3b5b762b411
KER AND HAN <shalcozbi@gmail.com>
2025-04-01 23:36:40 +0800

    ask user for input

--- batch_7/add_zeros_at_beginning.py
+++ batch_7/add_zeros_at_beginning.py
@@ -1,6 +1,8 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
 #Ask user for input
-#Ask user for length
-#Add zeros if length entered is shorter than the string
+#Ask user for width
+#Add zeros if width entered is shorter than the string
 #Print result
+
+text = input("Enter text: ")
