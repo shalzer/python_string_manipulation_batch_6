5236cb2374aed2080b89017c93363c033432e41a
KER AND HAN <shalcozbi@gmail.com>
2025-04-01 23:33:42 +0800

    pseudocode b7 p7

---
+++ batch_7/add_zeros_at_beginning.py
@@ -0,0 +1,6 @@
+# Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+
+#Ask user for input
+#Ask user for length
+#Add zeros if length entered is shorter than the string
+#Print result
