7bda45191d65b9502ab9b6645b0378ecc097e54e
KER AND HAN <shalcozbi@gmail.com>
2025-04-02 00:51:27 +0800

    pseudocode batch 7 prog 8

---
+++ batch_7/count_occurrence.py
@@ -0,0 +1,7 @@
+#Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+#Ask user for input
+#Ask user for keyword/s to find
+#Initialize counter to track occurrence
+#loop through input
+#Print result
